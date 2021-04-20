01ed887eb5bf7b676fb97c9b1c937a81feaafdd6
Dan Aita <59552043+aitaDev@users.noreply.github.com>
2021-04-19 19:00:52 -0500

    added single user info

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export class User extends Component {
 	componentDidMount() {
 		this.props.getUser(this.props.match.params.login);
 	}
+
+	static propTypes = {
+		loading: PropTypes.bool,
+		user: PropTypes.object.isRequired,
+		getUser: PropTypes.func.isRequired,
+	};
 
 	render() {
 		const {
@@ -18,11 +27,76 @@ export class User extends Component {
 			public_repos,
 			public_gists,
 			hireable,
+			company,
+			blog,
 		} = this.props.user;
 
 		const { loading } = this.props;
 
-		return <div>{name}</div>;
+		if (loading) return <Spinner />;
+
+		return (
+			<>
+				<Link to='/' className='btn btn-light'>
+					Back to Search
+				</Link>
+				Hireable: {''}
+				{hireable ? (
+					<i className='fas fa-check text-success' />
+				) : (
+					<i className='fas fa-times-circle text-danger' />
+				)}
+				<div className='card grid-2'>
+					<div className='all-center'>
+						<img
+							src={avatar_url}
+							className='round-img'
+							alt=''
+							style={{ width: '150px' }}
+						/>
+						<h1>{name}</h1>
+						<p>{location}</p>
+					</div>
+					<div>
+						{bio && (
+							<>
+								<h3>{bio}</h3>
+							</>
+						)}
+						<a href={html_url} className='btn btn-dark my-1'>
+							Visit Github Profile
+						</a>
+						<li>
+							{login && (
+								<>
+									<strong>Username: {login}</strong>
+								</>
+							)}
+						</li>
+						<li>
+							{company && (
+								<>
+									<strong>Company: {company}</strong>
+								</>
+							)}
+						</li>
+						<li>
+							{blog && (
+								<>
+									<strong>Website: {blog}</strong>
+								</>
+							)}
+						</li>
+					</div>
+				</div>
+				<div className='card text-center'>
+					<div className='badge badge-primary'>Followers: {followers}</div>
+					<div className='badge badge-success'>Following: {following}</div>
+					<div className='badge badge-light'>Public Repos: {public_repos}</div>
+					<div className='badge badge-dark'>Public Gists: {public_gists}</div>
+				</div>
+			</>
+		);
 	}
 }
 
